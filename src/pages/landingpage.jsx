9669cfe7eea2fe82a1eb85dcaedec77369e6931b
ulloa <ulloadario11@gmail.com>
2026-04-29 13:27:15 -0400

    Landing page

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -1,1 +1,15 @@
+import React, { Component } from 'react';
 
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <div className="container mt-5">
+                <div className="p-5 mb-4 bg-light rounded-3 border">
+                    <div className="container-fluid py-5">
+                        <h1 className="display-5 fw-bold">Welcome to the Home Depot</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
