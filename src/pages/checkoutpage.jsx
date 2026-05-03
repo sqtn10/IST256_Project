6e321f140940f145d6e959e4094694508b767393
tjg6087 <tjg6087@psu.edu>
2026-05-02 23:31:26 -0400

    Update checkoutpage.jsx

--- src/pages/checkoutpage.jsx
+++ src/pages/checkoutpage.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+<Link to ="/shoppingcart"/>
 
 export default class CheckoutPage extends Component {
     render() {
